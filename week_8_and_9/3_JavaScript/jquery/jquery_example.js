f662ef462a7e12a8c6d85e31ed16c3c8db6b990e
Natalie McCroy <nataliemccroy@gmail.com>
2014-05-11 21:54:30 -0500

    query challenge updates

--- week_8_and_9/3_JavaScript/jquery/jquery_example.js
+++ week_8_and_9/3_JavaScript/jquery/jquery_example.js
@@ -7,15 +7,25 @@ $('body').css({'background-color': 'pink'})
  
 //RELEASE 1:
   //Add code here to select elements of the DOM 
+  var bodyElement = $('body')
  
  
 //RELEASE 2: 
   // Add code here to modify the css and html of DOM elements
+  $('h1').css({'color': 'grey'})
+  $('h1').css({'border': 'solid'})
+  $('h1').css({'visibility': 'initial'})
+  $( "div.mascot" )
+  .html( "<h1>Fireflies!</h1>" );
  
  
 //RELEASE 3: Event Listener
   // Add the code for the event listener here 
  
+$('img').on('mouseover', function(e){
+     e.preventDefault()
+    $(this).attr('src', 'http://www.sparkthemagazine.com/wp-content/uploads/2013/01/fireflies-jar.jpg')
+  })
  
 //RELEASE 4 : Experiment on your own
  
