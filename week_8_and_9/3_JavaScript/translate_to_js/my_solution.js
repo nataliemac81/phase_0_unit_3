8796ad59b2c5802703d4141b8d190ab60f68ee44
nataliemac81 <nataliemccroy@gmail.com>
2014-05-11 22:25:14 -0500

    Update my_solution.js

--- week_8_and_9/3_JavaScript/translate_to_js/my_solution.js
+++ week_8_and_9/3_JavaScript/translate_to_js/my_solution.js
@@ -1,13 +1,39 @@
 // U3.W8-9: Challenge you're converting
 
-
-// I worked on this challenge [by myself, with: ].
+// Super Fizzbuzz
+// I worked on this challenge by myself.
 
 // 2. Pseudocode
-
+//def super_fizzbuzz(array)
+	//array.collect do |i|
+    	//if i % 15 == 0
+    		//"FizzBuzz"
+    	//elsif i % 5 == 0
+    		//"Buzz"
+    	//elsif i % 3 == 0
+    		//"Fizz"
+    	//else
+      //		i
+    //	end
+  //	end
+//end
 
 
 // 3. Initial Solution
+
+def super_fizzbuzz(array)
+	array.collect do |i|
+    	if i % 15 == 0
+    		"FizzBuzz"
+    	elsif i % 5 == 0
+    		"Buzz"
+    	elsif i % 3 == 0
+    		"Fizz"
+    	else
+      		i
+    	end
+  	end
+end
 
 
 
@@ -19,7 +45,13 @@
 
 
 
+
 // 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
+
+puts super_fizzbuzz([1,2,3]) == [1,2,'Fizz']
+puts super_fizzbuzz([1,2,5]) == [1, 2, "Buzz"]
+puts super_fizzbuzz([1,2,15]) == [1, 2, "FizzBuzz"]
+puts super_fizzbuzz([15, 5, 3, 1]) == ['FizzBuzz', 'Buzz', 'Fizz', 1]
 
 
 
